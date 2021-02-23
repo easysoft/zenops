0478c02b05c9bfcb9503e7d28e0112ba62ac8a34
陈琦 <chenqi@easycorp.ltd>
2021-02-23 11:08:21 +0800

    init

--- pms/build/build.sh
+++ pms/build/build.sh
@@ -1,4 +1,4 @@
 cd /Users/aaron/rd/project/zentao/php/work/zentaopms
 git fetch --all && git reset --hard origin/master && git pull
-sed  -i '' 144,147d Makefile
+
 make
--- pms/build/docker_init.sh
+++ pms/build/docker_init.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 set -e
+export Z_HOME=/z
 
 # compile
-
+sed -i '145,148s/^/#&/' $Z_HOME/zentaopms/Makefile
+cd $Z_HOME/zentaopms
+make pms
+cd $Z_HOME
 
 touch /tmp/daemon.log
 tail -f /tmp/daemon.log
