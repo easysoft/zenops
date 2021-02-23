2f1bad90e08f25079913203737a5be311b19cb50
陈琦 <chenqi@easycorp.ltd>
2021-02-23 12:07:36 +0800

    close task#36182

--- pms/build/docker_init.sh
+++ pms/build/docker_init.sh
@@ -6,8 +6,12 @@ export Z_HOME=/z
 sed -i '145,148s/^/#&/' $Z_HOME/zentaopms/Makefile
 cd $Z_HOME/zentaopms
 make pms
+
+# upload
+BUILD_NAME=$(ll *.zip | awk '{print $NF}')
+curl -v -k -u zenops-user:P2ssw0rd  -T $BUILD_NAME  http://192.168.1.161:58082/artifactory/zentaopms/$BUILD_NAME
+
 cd $Z_HOME
 
 touch /tmp/daemon.log
 tail -f /tmp/daemon.log
-
