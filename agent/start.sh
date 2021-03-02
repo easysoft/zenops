43d47cc9f8960b5d24e79a0d9134f2ebe1002ba0
陈琦 <chenqi@easycorp.ltd>
2021-03-02 14:25:24 +0800

    update zenops agent

--- agent/start.sh
+++ agent/start.sh
@@ -4,6 +4,4 @@ echo "install and start zmanager ..."
 zmanager -a start -l zh
 
 echo "start jenkins-agent ..."
-jenkins-agent -url $1 $2 $3 # jenkins url, token and agent name
-
-su - oracle -c "ulimit -aHS"
+jenkins-agent $1 $2 $3 $4 # '-url', jenkins url, token and agent name
