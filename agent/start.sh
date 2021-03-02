471f5bc1ab0baed8de374169c38d3fcff918a7a2
陈琦 <chenqi@easycorp.ltd>
2021-03-02 17:22:27 +0800

    update zenops agent apt src

--- agent/start.sh
+++ agent/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "install and start zmanager ..."
-zmanager -a start -l zh --ztf --zd
+zmanager -a start
 
 echo "start jenkins-agent ..."
 jenkins-agent $1 $2 $3 $4 # '-url', jenkins url, token and agent name
