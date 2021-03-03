2b424422be580fdd04f59858de2af20a9d2e7d29
陈琦 <chenqi@easycorp.ltd>
2021-03-03 15:34:23 +0800

    update zenops agent apt src

--- zentao/build/docker_init.sh
+++ zentao/build/docker_init.sh
@@ -2,16 +2,13 @@
 set -e
 export Z_HOME=/z
 
-GIT_ACCOUNT="$ENV_GIT_USER:$ENV_GIT_TOKEN"
-echo "GIT_ACCOUNT=$GIT_ACCOUNT"
-
-ARTIFACTORY_ACCOUNT="$ENV_ARTIFACTORY_USER:$ENV_ARTIFACTORY_PASSWORD"
-echo "ARTIFACTORY_ACCOUNT=$ARTIFACTORY_ACCOUNT"
+echo "GIT: $ENV_GIT_USER:$ENV_GIT_TOKEN@$GIT_SERVER"
+echo "ARTIFACTORY: $ENV_ARTIFACTORY_USER:$ENV_ARTIFACTORY_PASSWORD@$ARTIFACTORY_OSS_SERVER"
 
 # pull codes
 echo "start pull codes @ $(date +%T)"
 cd $Z_HOME
-git clone "http://$GIT_ACCOUNT@192.168.1.161:51080/root/zentaopms.git"
+git clone "http://$ENV_GIT_USER:$ENV_GIT_TOKEN@$GIT_SERVER/root/zentaopms.git"
 echo "end pull codes @ $(date +%T)"
 
 # edit makefile
@@ -23,7 +20,7 @@ make pms
 
 # upload
 BUILD_NAME=$(ls -l *.zip | awk '{print $NF}')
-curl -v -k -u "$ARTIFACTORY_ACCOUNT" -T "$BUILD_NAME"  http://192.168.1.161:58082/artifactory/zentaopms/$BUILD_NAME
+curl -v -k -u "$ENV_ARTIFACTORY_USER:$ENV_ARTIFACTORY_PASSWORD" -T "$BUILD_NAME"  http://$ARTIFACTORY_OSS_SERVER/artifactory/zentaopms/$BUILD_NAME
 
 cd $Z_HOME
 
